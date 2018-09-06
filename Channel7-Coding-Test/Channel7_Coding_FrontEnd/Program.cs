8fab3f8b0259ab23b86accaf19598fbb67cf23c3
BalajiMariappan <mbala38@hotmail.com>
2018-09-06 13:54:42 +1000

    Final Commit

--- Channel7-Coding-Test/Channel7_Coding_FrontEnd/Program.cs
+++ Channel7-Coding-Test/Channel7_Coding_FrontEnd/Program.cs
@@ -10,24 +10,28 @@ namespace Channel7_Coding_FrontEnd
         {
             try
             {
-                int Age = 23; int Id = 62;
                 string JsonPath = ConfigurationManager.AppSetting["JsonFilePath:Path"];
+                Console.WriteLine("Enter Age");
+                int Age = Convert.ToInt32(Console.ReadLine());
                 string FirstNames = CommaSeparatedFirstNameByAge.GetCommaSeparatedFirstNameByAge(JsonPath, Age);
+                Console.WriteLine("Enter Id");
+                int Id = Convert.ToInt32(Console.ReadLine());
                 List<string> FullNameByIdResult = FullNameById.GetFullNameById(JsonPath, Id);
                 List<string> GroupByAgeandGenderResult = GroupByAgeandGender.GetGroupByAgeandGender(JsonPath);
-                Console.WriteLine("Get Comma Separated FirstName By Age" + " " + Age + " " + "Result");
+
+                Console.WriteLine("Get Comma Separated FirstName(s) for Age" + " " + Age + " " + "Result for all");
                 Console.WriteLine("-------------------------------------------------");
                 Console.WriteLine(FirstNames);
                 Console.WriteLine("-------------------------------------------------");
 
-                Console.WriteLine("Get Full Name By Id" + " " +  Id + " " + "Result");
+                Console.WriteLine("Get Full Name for Id" + " " + Id + " " + "Result");
                 Console.WriteLine("-------------------------------------------------");
                 foreach (string fnames in FullNameByIdResult)
                 {
                     Console.WriteLine(fnames);
                 }
                 Console.WriteLine("-------------------------------------------------");
-                Console.WriteLine("Get GroupBy Age and Gender Result");
+                Console.WriteLine("Get GroupBy Age and Gender Result for all");
                 Console.WriteLine("-------------------------------------------------");
                 foreach (string fnames in GroupByAgeandGenderResult)
                 {
